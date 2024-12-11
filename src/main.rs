e24d8424c1919034a8015bd7d0a2c95367ea54bc
Juan David <juan.diaz@23degrees.io>
2024-12-11 10:57:58 +0100

    UPDATE

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ use macroquad::math::Affine2;
 
 use entities::shape::Shape;
 
+enum GameState {
+    MainMenu,
+    Playing,
+    Paused,
+    GameOver,
+}
+
 #[macroquad::main("Isometric")]
 async fn main() {
     
@@ -21,6 +28,7 @@ async fn main() {
     };
 
     let mut gameover = false;
+    let mut game_state = GameState::MainMenu;
         
     loop {
         clear_background(WHITE);
@@ -29,44 +37,59 @@ async fn main() {
 
         if let Some(key) = get_last_key_pressed() {
             println!("{:?}", key);
-        }             
+        }
+
+        match game_state {
+            GameState::MainMenu => {
+                ...
+            }
+            GameState::Playing => {
+                let delta_time = get_frame_time();
+                if is_key_down(KeyCode::Right) {
+                    circle.x += MOVEMENT_SPEED * delta_time;
+                }
+                if is_key_down(KeyCode::Left) {
+                    circle.x -= MOVEMENT_SPEED * delta_time;
+                }
+                if is_key_down(KeyCode::Down) {
+                    circle.y += MOVEMENT_SPEED * delta_time;
+                }
+                if is_key_down(KeyCode::Up) {
+                    circle.y -= MOVEMENT_SPEED * delta_time;
+                }
+
+                circle.x = clamp(circle.x, 0.0, screen_width());
+                circle.y = clamp(circle.y, 0.0, screen_height());
+
+                // Generate a new square
+                if rand::gen_range(0, 99) >= 95 {
+                    let size = rand::gen_range(16.0, 64.0);
+                    squares.push(Shape {
+                        size,
+                        speed: rand::gen_range(50.0, 150.0),
+                        x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                        y: -size,
+                    });
+                }
+
+                // Move squares
+                for square in &mut squares {
+                    square.y += square.speed * delta_time;
+                }
+
+                // Remove squares below bottom of screen
+                squares.retain(|square| square.y < screen_height() + square.size);
+            }
+            GameState::Paused => {
+                ...
+            }
+            GameState::GameOver => {
+                ...
+            }
+        }
 
         if !gameover {
-            let delta_time = get_frame_time();
-            if is_key_down(KeyCode::Right) {
-                circle.x += MOVEMENT_SPEED * delta_time;
-            }
-            if is_key_down(KeyCode::Left) {
-                circle.x -= MOVEMENT_SPEED * delta_time;
-            }
-            if is_key_down(KeyCode::Down) {
-                circle.y += MOVEMENT_SPEED * delta_time;
-            }
-            if is_key_down(KeyCode::Up) {
-                circle.y -= MOVEMENT_SPEED * delta_time;
-            }
-
-            circle.x = clamp(circle.x, 0.0, screen_width());
-            circle.y = clamp(circle.y, 0.0, screen_height());
-
-            // Generate a new square
-            if rand::gen_range(0, 99) >= 95 {
-                let size = rand::gen_range(16.0, 64.0);
-                squares.push(Shape {
-                    size,
-                    speed: rand::gen_range(50.0, 150.0),
-                    x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
-                    y: -size,
-                });
-            }
-
-            // Move squares
-            for square in &mut squares {
-                square.y += square.speed * delta_time;
-            }
-
-            // Remove squares below bottom of screen
-            squares.retain(|square| square.y < screen_height() + square.size);
+            
         }        
 
         // Check for collisions
